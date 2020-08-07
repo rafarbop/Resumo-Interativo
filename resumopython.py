f5c0cb9e8ce228d15eccedc922acfb7b4ed8f64f
rafarbop <67751655+rafarbop@users.noreply.github.com>
2020-08-07 19:26:11 -0300

    Iniciado resumo entra da e saida de dados

--- resumopython.py
+++ resumopython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os, sys
+import subprocess, sys
 
 # Resumo de Python através de um menu no terminal
 # com opções qual assunto deseja ver o resumo.
@@ -17,14 +17,23 @@ def titulo(wellcome):
             break
     print("\n* ",wellcome.title()," *",sep='', end='\n\n')
 
+def limpar_tela():
+    retorno_limpar_tela = subprocess.run(['clear'])
+
 def input_output_basics():
-    os.system('clear')
+    limpar_tela()
     titulo("Demostrações Básicas de Entrada e Saída de Dados em Python:")
+    print("Em Python3 usamos a função print() para exibir valores, palavras ou o retorno de outra função na saida padrão.")
+    print("\tExemplo de código:")
+    print('\t'+(len('print(2)')*2+4)*'-')
+    print('\t>>> print(2)')
+    print('\t{}'.format(2))
+    print('\t'+(len('print(2)')*2+4)*'-')
     print("CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO\n")
     saida_resumo = input("Deseja finalizar o programa ou voltar ao menu inicial?\n0 - Finalizar\n1 - Voltar ao Menu Inicial\n:")
     print()
     if saida_resumo == '0':
-        os.system('clear')
+        limpar_tela()
         sys.exit()
     if saida_resumo == '1':
         start_menu()
@@ -34,13 +43,13 @@ def input_output_basics():
         input_output_basics()
 
 def var_basics():
-    os.system('clear')
+    limpar_tela()
     titulo("Veremos como utilizamos variáveis e quais os tipos existentes em python:")
     print("CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO\n")
     saida_resumo = input("Deseja finalizar o programa ou voltar ao menu inicial?\n0 - Finalizar\n1 - Voltar ao Menu Inicial\n:")
     print()
     if saida_resumo == '0':
-        os.system('clear')
+        limpar_tela()
         sys.exit()
     if saida_resumo == '1':
         start_menu()
@@ -50,13 +59,13 @@ def var_basics():
         var_basics()
 
 def operation_basics():
-    os.system('clear')
+    limpar_tela()
     titulo("Vamos rever quais são e como funcionam as principais operações básicas com variáveis em python:")
     print("CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO\n")
     saida_resumo = input("Deseja finalizar o programa ou voltar ao menu inicial?\n0 - Finalizar\n1 - Voltar ao Menu Inicial\n:")
     print()
     if saida_resumo == '0':
-        os.system('clear')
+        limpar_tela()
         sys.exit()
     if saida_resumo == '1':
         start_menu()
@@ -66,13 +75,13 @@ def operation_basics():
         operation_basics()
 
 def not_definied():
-    os.system('clear')
+    limpar_tela()
     titulo("Menu ainda em desenvolvimento - Wait a little longer!")
     print("CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO","CONTEUDO\n")
     saida_resumo = input("Deseja finalizar o programa ou voltar ao menu inicial?\n0 - Finalizar\n1 - Voltar ao Menu Inicial\n:")
     print()
     if saida_resumo == '0':
-        os.system('clear')
+        limpar_tela()
         sys.exit()
     if saida_resumo == '1':
         start_menu()
@@ -83,7 +92,7 @@ def not_definied():
 
 
 def start_menu():
-    os.system('clear')
+    limpar_tela()
     titulo("Bem Vindo ao Resumo de Python feito com Python")
     print("Escolha qual assunto deseja revisar os principais conceitos e funções:\n")
     print("\t1 - Saída e Entrada de Dados - Básico")
@@ -110,7 +119,7 @@ def start_menu():
 
 start_menu()
 
-os.system('clear')
+limpar_tela()
 
 #print("Voçe digitou a opção {}!".format(option_menu))
 #print(f"Voçe digitou a opção {option_menu}!")
